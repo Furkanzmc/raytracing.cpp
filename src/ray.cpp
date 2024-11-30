48cacfc578506a3cf7855d2da8301436b0ed9ba2
Furkan Uzumcu <furkanuzumcu@gmail.com>
2024-11-30 17:27:19 -0500

    6.1-2: Surface normals

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,16 +1,20 @@
 #include "ray.h"
 
 namespace {
-bool is_in_sphere(const ray_t &ray, const point3 &center, double radius)
+double hit_sphere(const ray_t &ray, const point3 &center, double radius)
 {
     const vec3 oc{center - ray.origin};
 
-    const auto a = vec::dot(ray.direction, ray.direction);
-    const auto b = -2.0 * vec::dot(ray.direction, oc);
-    const auto c = vec::dot(oc, oc) - radius * radius;
+    const auto a = ray.direction.length_squared();
+    const auto h = vec::dot(ray.direction, oc);
+    const auto c = oc.length_squared() - (radius * radius);
 
-    const auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    const auto discriminant = h * h - a * c;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+
+    return (h - std::sqrt(discriminant)) / a;
 }
 } // namespace
 
@@ -22,8 +26,12 @@ point3 position(const ray_t &ray, double t)
 
 color color_at(const ray_t &ray)
 {
-    if (is_in_sphere(ray, point3{0, 0, -1}, 0.5)) {
-        return color{1, 0, 0};
+    {
+        const auto t = hit_sphere(ray, point3{0, 0, -1}, 0.5);
+        if (t > 0.0) {
+            const auto N = vec::unit_vector(ray::position(ray, t) - vec3{0, 0, -1});
+            return 0.5 * color{N.x() + 1, N.y() + 1, N.z() + 1};
+        }
     }
 
     const vec3 unit_direction = vec::unit_vector(ray.direction);
